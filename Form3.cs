2c8064729b5888a200e262d8df2fba5b210933d7
Moqll <chichiwang2008@gmail.com>
2017-03-30 08:04:12 -0700

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusProject
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            Wumpus wump = new Wumpus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Wumpus wump = new Wumpus();
+            wump.wumpusWakesUp(2,true);
+            String wumpusWakesUp = wump.getWumpusState();
+            textBox1.Text = wumpusWakesUp;
+
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Program (1).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form3 MyForm = new Form3();
+            Application.Run(MyForm);
+        }
+    }
+}
