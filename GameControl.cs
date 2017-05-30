5cf755552b4af070cfdce0b58381ee57b5d29dda
reneew7 <s-rewang@lwsd.org>
2017-05-29 20:37:49 -0700

    Add files via upload

--- GameControl.cs
+++ GameControl.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,83 +9,210 @@ namespace WumpusProject
 {
     class GameControl
     {
-        //initialize
         Wumpus wumpus;
-        UserInterface UI;
+        //UserInterface UI;
         Map map;
         static Trivia trivia;
         Cave cave;
-        
-        //player
-        int playerPosition;
-        char playerOption;
+        Player player;
+        //UserInterface ui;
 
-        //wumpus
-        int wumpusPosition;
-        String wumpusState;
+        Boolean alive;
 
-        //trivia
-        int questionNum;
-        String question;
-        String answer;
-        Boolean correct;
+        int questionNumbers;
 
+        int questionsCorrect;
+        List<bool> questionsRight;
+  
         public GameControl()
         {
-            //player
-            playerPosition = 0;
-            
-            //wumpus
-            wumpus = new Wumpus();
-            this.wumpusPosition = wumpus.getWumpusPosition();
-            this.wumpusState = wumpus.getWumpusState();
-            
-            //trivia
-            questionNum = 15;
-            question = "";
-            answer = "";
-            correct = true;
-
-            //cave
+            wumpus = new Wumpus();//int position, int turnsSinceWumpus, enum WumpusState state
+            //UI = new UserInterface(); //????
+            map = new Map();//roomNumberPlayer, roomNumberWumpus, roomNumberPit1, roomNumberPit2
+            trivia = new Trivia();//TriviaList, A-D, AnswerList
             cave = new Cave();
-            cave.initialize();
+            player = new Player();//arrows, coins, turns, playersPosition, highScore (all int)
+
+            alive = true;
+            questionNumbers = 10; //current number of questions in text file
+            questionsRight = new List<bool>();
+            int questionsCorrect = 0;
+           
+        }
+        public void takeTurn()
+        {
+            player.newTurn();
+        }
+        public String getNextQuestion()//for testing?
+        {
+            Random r = new Random();
+            setQuestionNumber(r.Next(1, getNumberOfQuestions()+1));
+            return Trivia.GetQuestion(questionNumbers);
+        }
+        public int getNumberOfQuestions()
+        {
+            return Trivia.GetNumberOfQuestions();
+        }
+        public String[] getAnswers()
+        {
+            return Trivia.GetAnswers(questionNumbers);
+        }
+        public bool getCorrectness(String userAnswer)//userAnswer from UI
+        {
+            int coins = player.getCoins();
+            coins--;
+            player.setCoins(coins);
+            bool correct = Trivia.CheckAnswer(userAnswer, questionNumbers);
+            if (correct)
+            {
+                questionsCorrect++;
+            }
+            return correct;
+        }
+        public int getQuestionsCorrect()
+        {
+            return questionsCorrect;
+        }
+        public bool winTriviaRound(int correctNeeded, int chances)//wumpus is 3/5, pit is 2/3, buy 2 arrows 2/3, buy 1 secret 2/3
+        {
+            bool win = true;
+            if (correctNeeded >= questionsCorrect)
+            {
+                win = true;
+            }
+            else
+            {
+                win = false;
+            }
+            questionsCorrect = 0;
+            return win;
+        }
+        public List<Boolean> getQuestionsRight()
+        {
+            return questionsRight;
+        }
+
+        public int getPlayerPosition()
+        {
+            return map.getRoomNumberPlayer();
+        }
+        public int getCoins()
+        {
+            return player.getCoins();
+        }
+
+        public int buyArrows()
+        {
+            player.buyArrow();
+            return player.getArrows();
+        }
+        public int getScore()
+        {
+            return player.getHighScore();
+        }
+
+        //getStats as int[] Score, Turns, Coins, Arrows
+        public int[] getStats()
+        {
+            int[] stats = new int[4];
+            stats[0] = player.getHighScore();
+            stats[1] = player.getTurns();
+            stats[2] = player.getCoins();
+            stats[3] = player.getArrows();
+            return stats;
+        }
+   
+        
+        public String getSecret()
+        {
+            return player.buySecret();
+        }
+        //public bool wantArrows(String userOption)//from UI
+        //{
+        //    bool buy = false;
+        //    if (userOption.ToLower().CompareTo("yes") == 0)
+        //    {
+        //        buy = true;
+        //    }
+        //    return buy;
+        //}
+        //public bool wantSecret()//from UI
+        //{
+        //    bool buy = true;
+        //    return buy;
+        //}
+
+
+        public int getPosPit1()
+        {
+            return map.getRoomNumberPit1();
+        }
+        public int getPosPit2()
+        {
+            return map.getRoomNumberPit2();
+        }
+        public int getPosBat1()
+        {
+            return map.getRoomNumberBat1();
+        }
+        public int getPosBat2()
+        {
+            return map.getRoomNumberBat2();
+        }
+        public bool pitEncounter()
+        {
+            bool pit = map.getRoomNumberPlayer() == getPosPit1() || map.getRoomNumberPlayer() == getPosPit2();
+            return pit;
+        }
+        public bool batEncounter()
+        {
+            bool bat = map.getRoomNumberPlayer() == getPosBat1() || map.getRoomNumberPlayer() == getPosBat2();
+            return bat;
+        }
+
+
+        public void wumpusEncounter()//will set wumpus state
+        {
+            wumpus.playerEnters(pitEncounter(), batEncounter(), cave);
+        }
+        public bool thereIsWumpus()
+        {
+            if(map.getRoomNumberWumpus() == map.getRoomNumberPlayer())
+            {
+                return true;
+            }
+            return false;
+        }
+        public String getWumpusState()
+        {
+            return wumpus.getWumpusState().ToString().ToLower();
         }
         
-        public void startGame() //user starts game
+        public String getWarning()
         {
-            String playerName = UI.playerName();
-            char wantNewGame = UI.wantNewGame();
-            char gameType = UI.typeOfGame();
-            int difficulty = UI.difficultyLevel();
-            map.newGame(map);
-            
+            return map.hazards(cave);
         }
-        public int getPlayerPosition() //player moves
+        public void setQuestionNumber(int number)//for testing
         {
-            playerPosition = UI.playerMoves();
-            return playerPosition;
+            questionNumbers = number;
         }
-
-        public char getPlayerOption() //player option
+        public int getQuestionNumber()
         {
-            return UI.userOptions();
+            return questionNumbers;
         }
-
-        public void encounter()
+        
+        public bool checkIfAlive()//alive
         {
-            playerPosition = getPlayerPosition();
-            wumpusPosition = wumpus.getWumpusPosition();
-            if (playerPosition == wumpusPosition)
+            if (player.getTurns() > 0)
             {
-                //fight? idek
+                if (player.getCoins() <= 0 || player.getArrows() <= 0)
+                {
+                    alive = false;
+                }
             }
+            return alive;
         }
 
-        public void goToTrivia() //trivia?
-        {
-            question = Trivia.GetQuestion(questionNum);
-            answer = Trivia.GetAnswer(questionNum);
-            correct = Trivia.CheckAnswer(answer, questionNum);
-        }
+
     }
 }
---
+++ Player.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusProject
+{
+    class Player
+    {
+        private int arrows;
+        private int coins;
+        private int turns;
+        private int playersPosition;
+        public int highScore;
+        public String alive;
+
+        Map map;
+
+        public Player()
+        {
+            arrows = 3;
+            coins = 0;
+            turns = 0;
+            playersPosition = 0;
+            highScore = 0;
+        }
+        public void newTurn()
+        {
+            if (coins <= 100)
+            {
+                coins++;
+            }
+            turns++;
+        }
+        public int getTurns()
+        {
+            return turns;
+        }
+        public int getCoins()
+        {
+            return coins;
+        }
+        public void setCoins(int newCoins)
+        {
+            coins = newCoins;
+        }
+        public void setPlayersPosition()
+        {
+            playersPosition = map.getRoomNumberPlayer();
+        }
+        public int getArrows()
+        {
+            if (arrows == 0)
+            {
+                death();
+            }
+            return arrows;
+        }
+        public void buyArrow()
+        {
+            coins--;
+            arrows += 2;
+        }
+        public int getHighScore()
+        {
+            return 100 - turns + coins + (10 * arrows);
+        }
+        public String buySecret()
+        {
+            String secret = map.buySecret();
+            return secret;
+        }
+        public void shotArrow()
+        {
+            arrows = map.shootArrow();
+        }
+        public void death()
+        {
+            getHighScore();
+            alive = "death";
+        }
+    }
+}
